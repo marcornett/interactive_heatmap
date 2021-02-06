1abfc9f8594880ad095b57d4a6aa1c7c9a1164cd
Marcel Cornett <marcornett@gmail.com>
2021-02-06 00:31:22 -0500

    Getting IP address from backend and providing to create map function.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const url = 'http://localhost:4000/token'
+const baseUrl = 'http://localhost:4000'
+const myMap = L.map('mapid').setView([0, 0], 2)
 
-const createMap = (token) =>{
-    const myMap = L.map('mapid').setView([51.505, -0.09], 13)
+const createMap = (token, data) => {
+    // TODO Create neccessary funcs 'addMarker', 'addCircle', 'addPolygon'
     L.tileLayer(
         'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
     {
@@ -22,16 +23,45 @@ const createMap = (token) =>{
     }).addTo(myMap)
 }
 
-const getAccessToken = async () => {
+const getDataIP = async () => {
+    let data
+    try{
+        const response = await fetch(`${baseUrl}/data`)
+        data = await response.json()
+    }catch(err){
+        console.error('Issue getting data for IP addresses.', err)
+    } finally{
+        return data
+    }
+}
+
+const main = async () => {
     let token
     try{    
-        const response = await fetch(url)
+        const response = await fetch(`${baseUrl}/token`)
         token = await response.text()
     } catch(err){
-        console.error(err)
+        console.error('Issue getting token for map.', err)
     } finally{
-        createMap(token)
+        let data = await getDataIP()
+        createMap(token, data)
     }
 } 
 
-getAccessToken()
+main()
+
+let marker = L.marker([51.5, -0.09]).addTo(myMap)
+let circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(myMap)
+let polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(myMap);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
