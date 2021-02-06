71e8525acf728f34aea3421971c16eb3e10aabab
Marcel Cornett <marcornett@gmail.com>
2021-02-06 00:34:03 -0500

    Getting IP address from backend and providing to create map function.

--- script.js
+++ script.js
@@ -49,19 +49,3 @@ const main = async () => {
 } 
 
 main()
-
-let marker = L.marker([51.5, -0.09]).addTo(myMap)
-let circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(myMap)
-let polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(myMap);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
