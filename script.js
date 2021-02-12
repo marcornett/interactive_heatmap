9b6a1facf69b1afb17481935d646af6961542aed
Marcel Cornett <marcornett@gmail.com>
2021-02-11 19:20:26 -0500

    Heatmap feature added. Currently, slowly runs based on JSON request.

--- script.js
+++ script.js
@@ -2,25 +2,35 @@ const baseUrl = 'http://localhost:4000'
 const myMap = L.map('mapid').setView([0, 0], 2)
 
 const createMap = (token, data) => {
-    // TODO Create neccessary funcs 'addMarker', 'addCircle', 'addPolygon'
-    L.tileLayer(
-        'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
-    {
-        attribution: `
-            Map data &copy; 
-            <a href="https://www.openstreetmap.org/copyright">
-                OpenStreetMap
-            </a> 
-            contributors, Imagery © 
-            <a href="https://www.mapbox.com/">
-                Mapbox
-            </a>`,
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: token
-    }).addTo(myMap)
+    let dataArr = []
+    data?.forEach(pair => {
+        dataArr.push(Object.values(pair))
+    })
+    
+    try {
+        L.tileLayer(
+            'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', 
+        {
+            attribution: `
+                Map data &copy; 
+                <a href="https://www.openstreetmap.org/copyright">
+                    OpenStreetMap
+                </a> 
+                contributors, Imagery © 
+                <a href="https://www.mapbox.com/">
+                    Mapbox
+                </a>`,
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: token
+        }).addTo(myMap)
+        
+        L.heatLayer(dataArr, {radius: 8}).addTo(myMap);
+    } catch (err) {
+        console.error('Issue creating map.', err)
+    }
 }
 
 const getDataIP = async () => {
